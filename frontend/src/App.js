f3dce357c6d96b00d1c938afa49e898389e4a16b
Shadowfied <admin@shadowfied.com>
2020-11-29 14:54:17 +0100

    feat: ✨ group posts into chunks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,27 @@
 import { useState, useEffect } from 'react';
 import TextPart from './TextPart';
+import _ from 'lodash';
 
 const App = () => {
-  const [posts, setPosts] = useState();
+  const [postChunks, setPostChunks] = useState();
 
   useEffect(() => {
     (async () => {
-      setPosts(await (await fetch('/api/posts')).json());
+      // TODO: Avoid lodash with a custom function?
+      setPostChunks(_.chunk(await (await fetch('/api/posts')).json(), 20));
     })();
   }, []);
-
   return (
     <div className='App' style={{ textAlign: 'justify' }}>
       <h1>GBAtemp One Word Novel Reader</h1>
-      {posts &&
-        posts.map((post, index) => {
-          const buf = [];
-
-          if (index && index % 20 === 0) {
-            buf.push(
-              <>
-                <br />
-                <br />
-              </>
-            );
-          }
-          buf.push(<TextPart post={post} />, ' ');
-          return buf;
-        })}
+      {postChunks &&
+        postChunks.map((posts) => (
+          <div key={Math.random()} style={{ marginBottom: '1rem' }}>
+            {posts.map((post) => (
+              <TextPart key={Math.random()} post={post} />
+            ))}
+          </div>
+        ))}
     </div>
   );
 };
