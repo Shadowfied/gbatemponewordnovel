4055e3dc2d65ada21dc2729877c275d7107cd2be
Shadowfied <admin@shadowfied.com>
2020-12-10 13:13:14 +0100

    fix: 🐛 actually fetch all new posts on every request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 // Setup express app
 const app = express();
 
-// Fetch any new posts. This is run on every request, but the majority of the job is already done at build time
+// Fetch any new posts.
 posts.initPosts();
 
 // Serve static assets from the build folder of the frontend
@@ -12,6 +12,9 @@ app.use(express.static(__dirname + '/frontend/build'));
 
 // Expose an endpoint to fetch the posts
 app.get('/api/posts', (req, res) => {
+  // Fetch any new posts. This is run on every request, but the majority of the job is already done at build time
+  posts.initPosts();
+
   res.send(JSON.stringify(posts.fetchAllPosts()));
 });
 
